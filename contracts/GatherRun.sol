338435fc7dc23e25974ce838aa347a6c9b2908ce
Gabriel Matei <gabrielvmatei@gmail.com>
2024-09-26 22:36:36 +0300

    gather runs crud

--- contracts/GatherRun.sol
+++ contracts/GatherRun.sol
@@ -6,5 +6,62 @@ import "@openzeppelin/contracts/utils/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract GatherRun is Ownable, Pausable {
+    uint256 public nextRunId = 1;
+
+    struct Drop {
+        uint256 resourceId;
+        uint16 minProbability;
+        uint16 maxProbability;
+    }
+
+    struct Run {
+        bool isDisabled;
+        uint64 duration;
+        uint256 avgDrop;
+        Drop[] drops;
+    }
+
+    mapping(uint256 => Run) public runs;
+
     constructor() Ownable(msg.sender) {}
+
+    function createRun(uint64 duration, uint256 avgDrop, Drop[] memory drops) public onlyOwner {
+        runs[nextRunId] = Run({
+            duration: duration,
+            avgDrop: avgDrop,
+            drops: drops,
+            isDisabled: false
+        });
+        nextRunId++;
+    }
+
+    function updateRun(uint256 runId, uint64 duration, uint256 avgDrop, Drop[] memory drops) public onlyOwner {
+        runs[runId] = Run({
+            duration: duration,
+            avgDrop: avgDrop,
+            drops: drops,
+            isDisabled: runs[runId].isDisabled
+        });
+    }
+
+    function disableRun(uint256 runId) public onlyOwner {
+        runs[runId].isDisabled = true;
+    }
+
+    function enableRun(uint256 runId) public onlyOwner {
+        runs[runId].isDisabled = false;
+    }
+
+    function totalRuns() public view returns (uint256) {
+        return nextRunId;
+    }
+
+    function getRun(uint256 runId) public view returns (Run memory) {
+        return runs[runId];
+    }
+
+    // startGatherRun - as user with nft
+    // sendAllEligibleToGather
+    // claimAllGatherRunRewards
+    // claimGatherRunRewards
 }
