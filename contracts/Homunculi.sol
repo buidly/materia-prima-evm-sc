2ca0d574737eecc81b768cd90c2058cf24642562
Gabriel Matei <gabrielvmatei@gmail.com>
2024-09-29 22:36:55 +0300

    create nft details

--- contracts/Homunculi.sol
+++ contracts/Homunculi.sol
@@ -6,14 +6,37 @@ import "@openzeppelin/contracts/utils/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MPResources is ERC721, Ownable, Pausable {
-    constructor() ERC721("MPHomunculi", "MPHOM") Ownable(msg.sender) {}
-
-    modifier canMint() {
-        // TODO
-        _;
+    struct NftDetails {
+        string name;
+        uint64 royalties;
     }
 
-    function mint(address to, uint256 tokenId) public canMint {
-        _mint(to, tokenId);
+    mapping(string => NftDetails) public nftDetails;
+    mapping(string => uint64) public nftTier;
+    mapping(string => uint256) public idLastMintedIndex;
+    mapping(string => string[]) public tags;
+    mapping(string => string) public mediaType;
+    mapping(string => uint256) public availableAssetsIds;
+    mapping(string => string) public collectionHash;
+
+    constructor() ERC721("MPHomunculi", "MPHOM") Ownable(msg.sender) {}
+
+    function setNftDetails(
+        string memory id,
+        string memory name,
+        string memory _collectionHash,
+        string[] memory _tags,
+        string memory _mediaType,
+        uint256 maxLen,
+        uint64 royalties,
+        uint64 tier
+    ) public onlyOwner {
+        nftDetails[id] = NftDetails({name: name, royalties: royalties});
+        nftTier[id] = tier;
+        idLastMintedIndex[id] = 0;
+        tags[id] = _tags;
+        mediaType[id] = _mediaType;
+        availableAssetsIds[id] = maxLen;
+        collectionHash[id] = _collectionHash;
     }
 }
