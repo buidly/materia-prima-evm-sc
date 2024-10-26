51db2ea1c8320d0af8e7da7851a9952f878678bf
Gabriel Matei <gabrielvmatei@gmail.com>
2024-10-26 19:40:45 +0300

    add upgrade scripts

--- tasks/homunculi.ts
+++ tasks/homunculi.ts
@@ -5,6 +5,12 @@ import fs from "fs";
 
 const homunculiScope = scope("homunculi", "Homunculi contract tasks");
 
+const getSetupConfig = (network: string, key: string) => {
+  const filename = "setup.config.json";
+  const config = JSON.parse(fs.readFileSync(filename, "utf8"));
+  return config[network][key];
+}
+
 const updateSetupConfig = (network: string, key: string, value: any) => {
   const filename = "setup.config.json";
   const config = JSON.parse(fs.readFileSync(filename, "utf8"));
@@ -37,6 +43,15 @@ homunculiScope.task("deploy", "Deploys Homunculi contract")
 homunculiScope.task("upgrade", "Upgrades Homunculi contract")
   .addOptionalParam("price", "Gas price in gwei for this transaction", undefined)
   .setAction(async (taskArgs, hre) => {
-    console.log("Upgrading Homunculi contract");
+    const [adminWallet] = await hre.ethers.getSigners();
+    console.log("Admin Public Address: ", adminWallet.address);
 
+    const homunculiAddress = getSetupConfig(hre.network.name, 'homunculi');
+    console.log("Homunculi deployed to: ", homunculiAddress);
+
+    console.log("Upgrading Homunculi contract");
+    const factory = (await hre.ethers.getContractFactory("Homunculi")).connect(adminWallet);
+    const upgraded = await hre.upgrades.upgradeProxy(homunculiAddress, factory); // no additional calls on the upgrade method
+
+    console.log("New Homunculi deployed to: ", upgraded.target);
   });
